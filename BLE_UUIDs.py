c525dd110b400159dc14c4e21fc11ebe2f3b99d4
Eddie <eddie.amaya@analog.com>
2022-08-03 13:37:00 -0500

    comment

--- BLE_UUIDs.py
+++ BLE_UUIDs.py
@@ -1,8 +1,10 @@
+class BLE_UUID():
 
-file = open("UUIDs.txt",'r')
-data = file.readlines()
-info_dict = {}
-for line in data:
-    line = line.split()
-    if line[1] not in info_dict:
-        info_dict[line[2]] = line[1]
+    file = open("UUIDs.txt",'r')
+    data = file.readlines()
+    uuid_dict = {}
+    #this should probably only happen once when the class is instantiated 
+    for line in data:
+        line = line.split()
+        if line[1] not in uuid_dict:
+            uuid_dict[line[2]] = line[1]
