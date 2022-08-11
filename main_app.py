93b9a3db02b4abeb948917bf2799ff02a0f8e97b
Eddie <eddie.amaya@analog.com>
2022-08-11 15:55:04 -0500

    removed unused method and uuid text file

--- main_app.py
+++ main_app.py
@@ -323,17 +323,6 @@ class MainInterface(QMainWindow):
     """
         self.ui.btnLabelPermissions.setText(lblPermissions)
     # ------------------------------------------------------------------------
-
-    def uuid_parse(self, uuid):
-        file = open("UUIDs.txt", 'r')
-        data = file.readlines()
-        uuid_dict = {}
-        # this should probably only happen once when the class is instantiated
-        for line in data:
-            line = line.split()
-            if line[1] not in uuid_dict:
-                uuid_dict[line[2]] = line[1]
-
     def discoveredList2ItemPressed(self):
         value = self.ui.list_discoveredDevices.currentItem()
         tmp = value.text()
