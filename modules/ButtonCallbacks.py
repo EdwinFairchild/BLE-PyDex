3af1fe767988297e2727342f1431add698389709
Eddie <eddie@edwinfairchild.com>
2022-08-19 23:15:04 -0500

    fixed button callback calling a list function

--- modules/ButtonCallbacks.py
+++ modules/ButtonCallbacks.py
@@ -57,71 +57,6 @@ def btn_type_copy(interface):
     MiscHelpers.copy_to_clipboard(interface,interface.ui.btnLabelType.text())
 
 # ------------------------------------------------------------------------
-def btn_tree_widget_item_pressed(interface):
-    # get text of seleced item
-    treeWidgetItemtext = interface.ui.servicesTreeWidget.currentItem()
-    # the 0 in text(0) means which column index. There is only 1 column used
-    dataList = treeWidgetItemtext.text(0).split(":")
-    dataListLen = len(dataList)
-    # get UUID
-    interface.ui.btnLabelType.setText(dataList[0])
-    lblUUID = dataList[1].split("(")
-    lblUUID[0].strip()
-    # get Handle
-    lblHandle = dataList[2].removesuffix(")")
-    lblHandle = lblHandle.strip()
-    interface.ui.btnLabelHandle.setText(lblHandle)
-    # get permissions
-    lblPermissions = "N/A"
-    # this will NEVER change
-    UUID_BLE_SPEC = ["0000", "1000", "8000", "00805f9b34fb"]
-
-    # this WILL change
-    UUID_name = lblUUID[0].strip()
-    UUID_val  = lblUUID[0].strip()
-    UUID = UUID_name.split("-")
-
-    tempUUID = UUID[0].removeprefix("0000").upper()
-    tempUUID = "0x"+tempUUID
-
-    if UUID[1:] == UUID_BLE_SPEC:
-        # check BLE specification defined UUIDS
-        if tempUUID in interface.UUID_dict:
-            UUID_name = interface.UUID_dict[tempUUID]
-    else:
-
-        # Get the full uuid and get rid of the dashes
-        full_uuid = lblUUID[0].strip().replace("-", "")
-        # check to see if the uuid is in the dictionary
-        # if so UUID_name takes the value returned from
-        # the dictionary at the key full_uuid
-
-        if full_uuid in interface.user_uuid_dict:
-            UUID_name = interface.user_uuid_dict[full_uuid]
-        else :
-            UUID_name = "Uknown"
-    interface.ui.btnLabelUUID.setText(UUID_val)
-    interface.ui.btnLabelUUID_name.setText(UUID_name)
-    if "read" in treeWidgetItemtext.text(0):
-        lblPermissions = "READ"
-    if "write" in treeWidgetItemtext.text(0):
-        lblPermissions += " : WRITE"
-    if "notify" in treeWidgetItemtext.text(0):
-        lblPermissions += " : NOTIFY"
-    """ TODO : 
-        "broadcast",
-        "read",
-        "write-without-response",
-        "write",
-        "notify",
-        "indicate",
-        "authenticated-signed-writes",
-        "extended-properties",
-        "reliable-write",
-        "writable-auxiliaries",
-"""
-    interface.ui.btnLabelPermissions.setText(lblPermissions)
-# ------------------------------------------------------------------------
 def btn_connect(interface):
         # Establish and maintain Bleak connection
         if interface.connected_state == False:
@@ -224,7 +159,6 @@ def register_button_callbacks(interface):
     interface.ui.btnReadChar.clicked.connect(lambda state : btn_read_char(interface))
     interface.ui.btnWriteChar.clicked.connect(lambda  state : btn_write_char(interface))
     interface.ui.btnConnect.clicked.connect(lambda state : btn_connect(interface))
-    interface.ui.servicesTreeWidget.itemPressed.connect(lambda state : btn_tree_widget_item_pressed(interface))
     interface.ui.btnLabelType.clicked.connect(lambda state : btn_type_copy(interface))
     interface.ui.btnLabelUUID.clicked.connect(lambda state : btn_uuid_copy(interface))
     interface.ui.btnLabelPermissions.clicked.connect(lambda state : btn_permission_copy(interface))
