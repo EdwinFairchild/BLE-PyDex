9110b049ee5a89228ae82cc0479cd2dde1a66753
Eddie <eddie@edwinfairchild.com>
2023-04-28 20:37:32 -0500

    Adds stylesheet to checkbox

--- main_app.py
+++ main_app.py
@@ -1,4 +1,4 @@
-import faulthandler 
+import faulthandler
 from click import style
 from BLE_GUI import Ui_MainWindow
 from modules import ButtonCallbacks
@@ -6,11 +6,11 @@ from modules import ListCallbacks
 from modules import MiscHelpers
 from modules import BLE_functions as ble_ctl
 from modules import SerialThread as ser_ctl
-from modules import Console 
+from modules import Console
 from PyQt5 import Qt as qtw
 from PyQt5 import QtCore as qtc
-from PyQt5 import QtCore, QtGui, QtWidgets 
-from PyQt5.QtCore import  pyqtSignal
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtCore import pyqtProperty
 from PyQt5.QtWidgets import QPushButton
 from PyQt5.QtCore import *
@@ -33,9 +33,10 @@ QtWidgets.QApplication.setAttribute(
 QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
 os.environ["QT_FONT_DPI"] = "96"
 
+
 class MainInterface(QMainWindow):
     # TODO : cleanup unused
-            
+
     selected_address = None
     advertised_name = None
     connected_address = None
@@ -67,28 +68,26 @@ class MainInterface(QMainWindow):
         self.ui.setupUi(self)
         self.ui.frm_otas.setVisible(False)
 
-        #console_init(self)
+        # console_init(self)
         ListCallbacks.register_list_callbacks(self)
         ButtonCallbacks.register_button_callbacks(self)
         MiscHelpers.init_icons(self)
         logTextBox = Console.QTextEditLogger(self)
         logTextBox.logMessage.connect(self.logToTextbox)
         # You can format what is printed to text box
-        logTextBox.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s',"%H:%M:%S"))
+        logTextBox.setFormatter(logging.Formatter(
+            '%(asctime)s - %(levelname)s - %(message)s', "%H:%M:%S"))
         logging.getLogger().addHandler(logTextBox)
         # You can control the logging level
         logging.getLogger().setLevel(logging.INFO)
-        #logging.getLogger().setLevel(logging.DEBUG)
+        # logging.getLogger().setLevel(logging.DEBUG)
         logging.info("BLE-Pydex initialized")
         #logging.debug('damn, a bug')
         #logging.warning('that\'s not right')
-        #logging.error('foobar')
+        # logging.error('foobar')
 
-
-    def logToTextbox(self,data):
+    def logToTextbox(self, data):
         self.ui.console.append(data)
-
-        
 
     # ------------------------------------------------------------------------
     # def eventFilter(self, source, event):
@@ -101,24 +100,29 @@ class MainInterface(QMainWindow):
     # ------------------------------------------------------------------------
 
 ########################################################################################
+
+
 def exitFunc():
     global interface
     try:
         interface.bleLoop.disconnect_triggered = True
-        while interface.bleLoop.connect==True:
+        while interface.bleLoop.connect == True:
             pass
 
         # close any on running tasks
         for task in asyncio.all_tasks():
             task.cancel()
-    
+
     except Exception as e:
         print(e)
+
+
     # ------------------------------------------------------------------------
 if __name__ == '__main__':
     # todo: compile resurces into python files, not sure if its even necessary at this point
     # pyrcc5 image.qrc -o image_rc.py
     # compile gui
+    # pyuic5 -x BLE_GUI.ui -o BLE_GUI.py
     faulthandler.enable()
     os.system("pyuic5 -x BLE_GUI.ui -o BLE_GUI.py")
    # atexit.register(exitFunc)
